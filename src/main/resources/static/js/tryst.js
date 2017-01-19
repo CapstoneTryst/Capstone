383e23aee8ebd4a7e09e2eafe3e28e70cdfa8d38
Brad Colton <bradcolton11@yahoo.com>
2017-01-19 17:49:27 -0600

    modified to change the page more

--- src/main/resources/static/js/tryst.js
+++ src/main/resources/static/js/tryst.js
@@ -3,9 +3,13 @@ $(".categories").click(function() {
        location: "78209"
    })
        .done(function(data) {
-           $("#business-name").innerHTML = data.name;
-           document.getElementById("business-name").innerHTML = data.name;
-           console.log(data.name);
+           document.getElementById("businessTitle").innerHTML = data.name;
+           document.getElementById("reviewCount").innerHTML = data.review_count + " yelp reviews";
+           document.getElementById("businessID").innerHTML = data.phone;
+           document.getElementById("ratingImageUrl").innerHTML = data.rating_img_url;
+           document.getElementById("businessAddress").innerHTML = data.location.address[0];
+           document.getElementById("businessImage").innerHTML = data.image_url;
+
            console.log(data);
        });
 });
