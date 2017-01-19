09d9c1acf407946d19886d71fbbf2d65c0f809bb
Brad Colton <bradcolton11@yahoo.com>
2017-01-19 17:02:58 -0600

    created yelp api model

---
+++ src/main/java/com/codeup/api/Yelp.java
@@ -0,0 +1,64 @@
+package com.codeup.api;
+
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.scribe.builder.ServiceBuilder;
+import org.scribe.model.OAuthRequest;
+import org.scribe.model.Response;
+import org.scribe.model.Token;
+import org.scribe.model.Verb;
+import org.scribe.oauth.OAuthService;
+
+public class Yelp {
+
+    private static final String API_HOST = "api.yelp.com";
+    private static final int SEARCH_LIMIT = 3;
+    private static final String SEARCH_PATH = "/v2/search";
+    private static final String BUSINESS_PATH = "/v2/business";
+
+    OAuthService service;
+    Token accessToken;
+
+    /**
+     * Setup the Yelp API OAuth credentials.
+     *
+     * @param consumerKey Consumer key
+     * @param consumerSecret Consumer secret
+     * @param token Token
+     * @param tokenSecret Token secret
+     */
+    public Yelp(String consumerKey, String consumerSecret, String token, String tokenSecret) {
+        this.service =
+                new ServiceBuilder().provider(TwoStepOAuth.class).apiKey(consumerKey)
+                        .apiSecret(consumerSecret).build();
+        this.accessToken = new Token(token, tokenSecret);
+    }
+
+    public String searchForBusinessesByLocation(String term, String location) {
+        OAuthRequest request = createOAuthRequest(SEARCH_PATH);
+        request.addQuerystringParameter("term", term);
+        request.addQuerystringParameter("location", location);
+        request.addQuerystringParameter("limit", String.valueOf(SEARCH_LIMIT));
+        return sendRequestAndGetResponse(request);
+    }
+
+    public String searchByBusinessId(String businessID) {
+        OAuthRequest request = createOAuthRequest(BUSINESS_PATH + "/" + businessID);
+        return sendRequestAndGetResponse(request);
+    }
+
+    private OAuthRequest createOAuthRequest(String path) {
+        OAuthRequest request = new OAuthRequest(Verb.GET, "https://" + API_HOST + path);
+        return request;
+    }
+
+    private String sendRequestAndGetResponse(OAuthRequest request) {
+        this.service.signRequest(this.accessToken, request);
+        Response response = request.send();
+        return response.getBody();
+    }
+
+}
