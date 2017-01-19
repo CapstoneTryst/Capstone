c8b7da6be46a0409f2e1a67be90023b7ec0a82f7
Brad Colton <bradcolton11@yahoo.com>
2017-01-19 17:01:09 -0600

    added get method to return a json object

--- src/main/java/com/codeup/controllers/MainController.java
+++ src/main/java/com/codeup/controllers/MainController.java
@@ -1,22 +1,28 @@
 package com.codeup.controllers;
 
-import com.codeup.models.DateCategory;
+import com.codeup.api.YelpAPISvc;
+import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.Mapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
+
     @Autowired
-    private DateCategory categoriesDao;
+    YelpAPISvc yelpAPI;
 
     @GetMapping("/")
     public String showWelcome() {
         return "welcome";
+    }
+
+    @GetMapping("/show")
+    public @ResponseBody JSONObject showPage(@RequestParam(name = "location") String location, Model model) {
+        return yelpAPI.queryAPI(location);
     }
 
 //    @GetMapping("/{location}/{dateType}")
