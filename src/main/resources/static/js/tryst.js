3b6dc7586e87534e59b99066785d67c5048a2277
Brad Colton <bradcolton11@yahoo.com>
2017-01-20 13:45:20 -0600

    added events for the business search page

--- src/main/resources/static/js/tryst.js
+++ src/main/resources/static/js/tryst.js
@@ -1,17 +1,29 @@
 $(".date_type").click(function() {
    $.get("/show", {
-       location: "78209"
+       location: $("#location").val()
    })
        .done(function(data) {
            $("#businessTitle").html(data.name);
            $("#reviewCount").html(data.review_count + " yelp reviews");
            $("#businessPhone").html(data.display_phone);
            $("#ratingImageUrl").attr("src", data.rating_img_url);
-           $("#businessAddress").html(data.location.display_address[0] + ", " + data.location.display_address[2]);
+           $("#businessAddress").html(data.location.address[0] + "| " + data.location.city + ", " + data.location.state_code + " " + data.location.postal_code);
            $("#businessImage").attr("src", data.image_url);
 
            $("#showPage").css({"display": "inline"});
 
            console.log(data);
+       });
+});
+
+$("#businessButton").click(function() {
+   $.get("/business", {
+       name: $("#businessTerm").val(),
+       location: $("#businessLocation").val()
+   })
+       .done(function(data) {
+          $.each(data, function(index, element) {
+            $("#businessResults").append("<h1>" + element.name + "</h1>")
+          });
        });
 });
