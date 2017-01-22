84230ea987b4a3ff74b2a223299a3113cfc3f1dc
Austin Meeker <austinmeeker@me.com>
2017-01-22 12:28:46 -0600

    • @GetMapping("/business/new/{businessId}") pushes yelpBusinessID & current user ID to tryst_rankings table.

--- src/main/java/com/codeup/controllers/BusinessController.java
+++ src/main/java/com/codeup/controllers/BusinessController.java
@@ -1,14 +1,26 @@
 package com.codeup.controllers;
 
+import com.codeup.dao.TrystRankings;
+import com.codeup.models.TrystRanking;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
-public class BusinessController {
+public class BusinessController extends BaseController {
+
+    @Autowired
+    TrystRankings rankingsDoa;
+
     @GetMapping("/business/new/{businessId}")
     public String addNewBusiness(@PathVariable String businessId) {
+
+        TrystRanking newRanking = new TrystRanking();
+
+        newRanking.setYelpId(businessId);
+        newRanking.setUser(loggedInUser());
+        rankingsDoa.save(newRanking);
 
         return "redirect:/";
     }
