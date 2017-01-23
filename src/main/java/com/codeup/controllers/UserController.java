ca9e78e12ba63cdcd3c34d70b34b61aa2ae3a4e2
Brad Colton <bradcolton11@yahoo.com>
2017-01-23 13:53:45 -0600

    modified urls and created user ranking mappings

--- src/main/java/com/codeup/controllers/UserController.java
+++ src/main/java/com/codeup/controllers/UserController.java
@@ -1,7 +1,12 @@
 package com.codeup.controllers;
 
+import com.codeup.api.YelpAPISvc;
+import com.codeup.dao.TrystRankings;
 import com.codeup.dao.Users;
+import com.codeup.models.TrystRanking;
 import com.codeup.models.User;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
@@ -9,31 +14,40 @@ import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
-public class UserController {
+public class UserController extends BaseController {
 
     @Autowired
     private Users usersDao;
 
     @Autowired
+    private TrystRankings rankingsDao;
+
+    @Autowired
+    YelpAPISvc yelpAPI;
+
+    @Autowired
     private PasswordEncoder passwordEncoder;
 
-    @GetMapping("/login")
+    @GetMapping("/user/login")
     public String showLoginPage() {
         return "user/login";
     }
 
 
-    @GetMapping("/create")
+    @GetMapping("/user/create")
     public String createNewUser(Model model) {
         model.addAttribute("user", new User());
         return "user/create";
     }
 
-    @PostMapping("/create")
+    @PostMapping("/user/create")
     public String registerNewUser(@Valid User newUser, Errors validation, Model model){
 
         if (validation.hasErrors()){
@@ -45,6 +59,37 @@ public class UserController {
         newUser.setPassword(passwordEncoder.encode(newUser.getPassword()));
         usersDao.save(newUser);
         return "redirect:/login";
+    }
+
+    @GetMapping("/user/unrated")
+    public @ResponseBody List<JSONObject> showBusinessesToRate() {
+        List<TrystRanking> allBusinessesByUser = rankingsDao.findByUser(loggedInUser());
+        List<TrystRanking> unratedBusinesses = new ArrayList<>();
+        for(TrystRanking ranking : allBusinessesByUser) {
+            if (ranking.getRating() == 0) {
+                unratedBusinesses.add(ranking);
+            }
+        }
+        List<JSONObject> toBeRated = new ArrayList<>();
+        JSONObject toAdd;
+        for(TrystRanking ranking : unratedBusinesses) {
+            toAdd = yelpAPI.queryAPIByBusinessId(ranking.getYelpId());
+            toBeRated.add(toAdd);
+        }
+
+
+        return toBeRated;
+    }
+
+    @GetMapping("/test")
+    public @ResponseBody TrystRanking test() {
+        System.out.println(rankingsDao.findOne(17L));
+        return rankingsDao.findOne(17L);
+    }
+
+    @GetMapping("/user/rate")
+    public String showRatePage(Model model) {
+        return "user_rate_page";
     }
 
 
