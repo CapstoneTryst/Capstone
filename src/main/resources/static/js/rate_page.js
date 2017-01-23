1397400e16b63a904e7e163ebacafa9235e1af9b
Brad Colton <bradcolton11@yahoo.com>
2017-01-23 17:28:05 -0600

    now creates forms for all 10 categories and creates event listeners to submit post requests

--- src/main/resources/static/js/rate_page.js
+++ src/main/resources/static/js/rate_page.js
@@ -1,12 +1,71 @@
-$.get("/user/unrated")
-    .done(function(data) {
-        $.each(data, function(index, element) {
-            console.log(element);
-            console.log(element.name);
-           $("body").append("<h1>" + element.name + "</h1>");
+(function() {
+    var ratingString = "";
+    $.get("/user/unrated")
+        .done(function (data) {
+            ratingString = "";
+            $.each(data, function (index, element) {
+                ratingString += "<h1>" + element.name + "</h1>";
+                for (var i = 1; i <= 10; i++) {
+                    ratingString +=
+                        "<div class='d-inline'>"
+                        + "<form class='stopform' method='post' action='/business/positive'>"
+                        + "<input type='hidden' name='_csrf' value='" + $("#csrf-token").attr("content") + "'>"
+                        + "<input type='hidden' name='category' value='" + i + "'>"
+                        + "<input type='hidden' name='businessId' value='" + element.id + "'>"
+                        + "<button class='submit-positive' type='submit'>+</button>"
+                        + "</form>"
+                        + "<p>category " + i + "</p>"
+                        + "<form class='stopform' method='post' action='/business/negative'>"
+                        + "<input type='hidden' name='_csrf' value='" + $("#csrf-token").attr("content") + "'>"
+                        + "<input type='hidden' name='category' value='" + i + "'>"
+                        + "<input type='hidden' name='businessId' value='" + element.id + "'>"
+                        + "<button class='submit-negative' type='submit'>-</button>"
+                        + "</form>"
+                        + "</div>"
+                }
+
+            });
+            $("#businesses-to-rate").html(ratingString);
+
+            $(".stopform").submit(function (e) {
+                e.preventDefault();
+            });
+
+            $(".submit-positive").click(function() {
+               $.ajax({
+                   method: "POST",
+                   url: "/business/positive",
+                   data: $(this).closest("form").serialize()
+               })
+                   .done(function(data) {
+                       console.log(data);
+                       console.log("This is done")
+                   })
+                   .error(function(error) {
+                       console.log(error);
+                       console.log("This failed");
+                   })
+            });
+            $(".submit-negative").click(function() {
+                $.ajax({
+                    method: "POST",
+                    url: "/business/negative",
+                    data: $(this).closest("form").serialize()
+                })
+                    .done(function(data) {
+                        console.log(data);
+                        console.log("This is done")
+                    })
+                    .error(function(error) {
+                        console.log(error);
+                        console.log("This failed");
+                    })
+            });
+            console.log(data);
+        })
+        .fail(function (error) {
+            console.log(error);
         });
-        console.log(data);
-    })
-    .fail(function(error) {
-       console.log(error);
-    });
+
+
+})();
