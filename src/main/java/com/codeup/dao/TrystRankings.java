55cb51ada14680fb0399bbb54b9786d484951c0d
Brad Colton <bradcolton11@yahoo.com>
2017-01-23 13:52:46 -0600

    added findByUser method

--- src/main/java/com/codeup/dao/TrystRankings.java
+++ src/main/java/com/codeup/dao/TrystRankings.java
@@ -1,10 +1,14 @@
 package com.codeup.dao;
 
 import com.codeup.models.TrystRanking;
+import com.codeup.models.User;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
 
 /**
  * Created by Austin on 1/17/17.
  */
 public interface TrystRankings extends CrudRepository<TrystRanking, Long> {
+    List<TrystRanking> findByUser(User user);
 }
