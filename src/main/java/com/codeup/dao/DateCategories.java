48e38e28d8645d70be92c0ae65f9933acedd0b5a
Brad Colton <bradcolton11@yahoo.com>
2017-01-24 14:40:39 -0600

    added find all that returns a list of categories

--- src/main/java/com/codeup/dao/DateCategories.java
+++ src/main/java/com/codeup/dao/DateCategories.java
@@ -3,9 +3,12 @@ package com.codeup.dao;
 import com.codeup.models.DateCategory;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Date;
+import java.util.List;
+
 /**
  * Created by Austin on 1/17/17.
  */
 public interface DateCategories extends CrudRepository<DateCategory, Long> {
-
+    List<DateCategory> findAll();
 }
