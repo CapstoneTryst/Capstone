dc7292034e045ea5786281215c9c46512a92301b
Austin Meeker <austinmeeker@me.com>
2017-01-23 20:41:33 -0600

    • Removed unused imports Yelp.java • Added @PostMapping methods to BusinessController

--- src/main/java/com/codeup/controllers/BusinessController.java
+++ src/main/java/com/codeup/controllers/BusinessController.java
@@ -1,18 +1,18 @@
 package com.codeup.controllers;
 
 import com.codeup.dao.TrystRankings;
+import com.codeup.models.DateCategory;
 import com.codeup.models.TrystRanking;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class BusinessController extends BaseController {
 
     @Autowired
-    TrystRankings rankingsDao;
+    private TrystRankings rankingsDao;
 
     @PostMapping("/business/new/{businessId}")
     public String addNewBusiness(@PathVariable String businessId) {
@@ -25,4 +25,58 @@ public class BusinessController extends BaseController {
 
         return "redirect:/";
     }
+
+    @PostMapping("/business/positive")
+    @ResponseStatus(value = HttpStatus.OK)
+    public void ratePositive(@RequestParam("category") long category, @RequestParam("businessId") String businessId) {
+
+        TrystRanking unrankedRating = rankingsDao.findByUserIdAndRatingAndYelpId(loggedInUser(), 0, businessId);
+
+            DateCategory dateCategory = new DateCategory();
+            dateCategory.setId(category);
+
+        if (unrankedRating.getDateCategory() == null){
+
+            TrystRanking newTrystRating = new TrystRanking();
+
+            newTrystRating.setYelpId(businessId);
+            newTrystRating.setRating(1);
+            newTrystRating.setDateCategory(dateCategory);
+            newTrystRating.setUser(loggedInUser());
+
+            rankingsDao.save(newTrystRating);
+        }
+
+        unrankedRating.setRating(1);
+        unrankedRating.setDateCategory(dateCategory);
+        rankingsDao.save(unrankedRating);
+    }
+
+
+    @PostMapping("/business/negative")
+    public void rateNegative(@RequestParam("category") int category, @RequestParam("businessId") String businessId) {
+
+        TrystRanking unrankedRating = rankingsDao.findByUserIdAndRatingAndYelpId(loggedInUser(), 0, businessId);
+
+        DateCategory dateCategory = new DateCategory();
+        dateCategory.setId(category);
+
+        if (unrankedRating.getDateCategory() == null){
+
+            TrystRanking newTrystRating = new TrystRanking();
+
+            newTrystRating.setYelpId(businessId);
+            newTrystRating.setRating(-1);
+            newTrystRating.setDateCategory(dateCategory);
+            newTrystRating.setUser(loggedInUser());
+
+            rankingsDao.save(newTrystRating);
+        }
+
+        unrankedRating.setRating(-1);
+        unrankedRating.setDateCategory(dateCategory);
+        rankingsDao.save(unrankedRating);
+    }
+
+
 }
--- src/main/java/com/codeup/dao/TrystRankings.java
+++ src/main/java/com/codeup/dao/TrystRankings.java
@@ -2,6 +2,7 @@ package com.codeup.dao;
 
 import com.codeup.models.TrystRanking;
 import com.codeup.models.User;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
@@ -10,5 +11,9 @@ import java.util.List;
  * Created by Austin on 1/17/17.
  */
 public interface TrystRankings extends CrudRepository<TrystRanking, Long> {
+
     List<TrystRanking> findByUser(User user);
+
+    TrystRanking findByUserIdAndRatingAndYelpId(User user, int rating, String yelpId);
+
 }
