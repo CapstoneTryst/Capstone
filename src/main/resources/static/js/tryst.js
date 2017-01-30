86b6ec3aea55dafad575cf137462cd81cb37cc85
Brad Colton <bradcolton11@yahoo.com>
2017-01-30 14:05:25 -0600

    added loading animation effects to the home page

--- src/main/resources/static/js/tryst.js
+++ src/main/resources/static/js/tryst.js
@@ -3,6 +3,7 @@ var category = "";
 
 $(".date_type").click(function() {
     category = $(this).data("db-id");
+    $("#wrapper").fadeIn();
    $.get("/show", {
        location: $("#location").val(),
        category: $(this).data("db-id")
@@ -24,7 +25,12 @@ $(".date_type").click(function() {
                    scrollTop: $('.second').offset().top},
                'slow');
            console.log(data);
-       });
+
+           $("#wrapper").fadeOut();
+       })
+       .error(function(error) {
+           $("#wrapper").fadeOut();
+       })
 });
 
 $("#businessButton").click(function() {
